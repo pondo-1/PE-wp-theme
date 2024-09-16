09fa84f5ea320be72d0740ba551c5118026f46a8
Diane Kang <diane.kang@page-effect.com>
2024-09-16 21:34:14 +0200

    update contactform button

--- functions.php
+++ functions.php
@@ -47,24 +47,25 @@ function allowedBlockTypes($original_allowedBlocks, $post)
 add_filter('allowed_block_types', 'allowedBlockTypes', 10, 2);
 
 
-// add classes to gravity form fields, see https://docs.gravityforms.com/gform_field_css_class/
-/* add_filter( 'gform_field_css_class', 'custom_class', 10, 3 );
-function custom_class( $classes, $field, $form ) {
-if ( $field->type == 'text' ) {
-$classes .= ' gradient-border-feature';
-}
-return $classes;
-} */
 
 //add class to gravity forms submit button, see https://docs.gravityforms.com/gform_submit_button/
-add_filter('gform_submit_button', 'add_custom_css_classes', 10, 2);
-function add_custom_css_classes($button, $form)
+
+add_filter('gform_submit_button', 'input_to_button', 10, 2);
+function input_to_button($button, $form)
 {
   $dom = new DOMDocument();
   $dom->loadHTML('<?xml encoding="utf-8" ?>' . $button);
   $input = $dom->getElementsByTagName('input')->item(0);
+  $new_button = $dom->createElement('button');
+  $new_button->appendChild($dom->createTextNode($input->getAttribute('value')));
+  $input->removeAttribute('value');
   $classes = $input->getAttribute('class');
-  $classes .= " btn--empty";
+  $classes .= " btn btn--empty";
   $input->setAttribute('class', $classes);
-  return $dom->saveHtml($input);
+  foreach ($input->attributes as $attribute) {
+    $new_button->setAttribute($attribute->name, $attribute->value);
+  }
+  $input->parentNode->replaceChild($new_button, $input);
+
+  return $dom->saveHtml($new_button);
 }
